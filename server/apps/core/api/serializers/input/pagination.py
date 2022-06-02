8a7ffea3b98f22f896f2d5f55ff28d0e593d47c7
Stanislav Lapshin <sm.lapshin@gmail.com>
2022-06-02 14:39:40 +0300

    refactor(code): remove obsolete code

--- server/apps/core/api/serializers/input/pagination.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-
-class PaginationMixin(serializers.Serializer):
-    """Mixin for pagination fields."""
-
-    page_size = serializers.IntegerField(required=False, min_value=1)
-    page = serializers.IntegerField(required=False, min_value=0)
